6165e927679a93100c9d7949468560d0ab2cd2d7
Dinh Cong Tan <f7deat@users.noreply.github.com>
2018-09-16 20:41:45 +0700

    navbar-active

--- f7deat.js
+++ f7deat.js
@@ -3,3 +3,7 @@ $('.avatar-image-container').addClass('rounded-circle');
 $('.comment-actions').children("span").children("a").addClass('btn btn-danger');
 $('.comment-actions').children("a").addClass('btn btn-info');
 $('.comment').addClass('list-group-item m-2');
+$('#comments').addClass('card mx-2');
+$('#comments').children("h4").addClass('card-header');
+//navbar
+$('.nav-item').children("span").addClass('py-2 bg-primary text-light d-flex px-4');
