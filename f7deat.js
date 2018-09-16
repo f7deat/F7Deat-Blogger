07bf9109fdc77af3178056551273d573ff287c9f
Dinh Cong Tan <f7deat@users.noreply.github.com>
2018-09-16 20:08:30 +0700

    add custom js

---
+++ f7deat.js
@@ -0,0 +1,5 @@
+//Comment
+$('.avatar-image-container').addClass('rounded-circle');
+$('.comment-actions').children("span").children("a").addClass('btn btn-danger');
+$('.comment-actions').children("a").addClass('btn btn-info');
+$('.comment').addClass('list-group-item m-2');
