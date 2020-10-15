de648f01900380bd309bd654050a06f74a42872b
Dinh Cong Tan <31816603+f7deat@users.noreply.github.com>
2020-10-15 17:52:52 +0700

    Update f7deat.js

--- f7deat.js
+++ f7deat.js
@@ -6,6 +6,6 @@ $('.comment').addClass('list-group-item m-2');
 $('#comments').addClass('card mx-2');
 $('#comments').children("h4").addClass('card-header');
 //navbar
-$('.nav-item').children("span").addClass('p-5 text-light');
+$('.nav-item').children("span").addClass('px-5 text-light p-3 d-block bg-primary');
 $('#btnSidebar').on('click',function(){$('.sidebar').toggleClass('d-none')});
 $('.navbar-toggler').on('click',function(){$('#brand').toggleClass('d-none');$('#menu').toggleClass('col-12')});
