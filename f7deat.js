4a37351ed23f043b788d0e9eefc90a58b26bb9eb
Dinh Cong Tan <31816603+f7deat@users.noreply.github.com>
2021-09-08 11:29:41 +0700

    remove margin comment wrapper

--- f7deat.js
+++ f7deat.js
@@ -3,7 +3,6 @@ $('.avatar-image-container').addClass('rounded-circle');
 $('.comment-actions').children("span").children("a").addClass('btn btn-danger');
 $('.comment-actions').children("a").addClass('btn btn-info');
 $('.comment').addClass('list-group-item m-2');
-$('#comments').addClass('card mx-2');
 $('#comments').children("h4").addClass('card-header');
 //navbar
 $('.nav-item').children("span").addClass('px-5 text-light p-3 d-block bg-primary');
